fc0ca5ae25b021243185053000be670233156a4b
Gabbylim <gabrianelim@gmail.com>
2020-07-03 07:59:47 +0300

    remove evironment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = 
-  {
-    :username => 'chattboxKE',
-    :api_key  => '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',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+
